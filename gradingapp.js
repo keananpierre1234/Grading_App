06dfa8ae792835f2a3745fe4950f59199ffa3908
keananpierre1234 <keananpierrekj@hotmail.com>
2015-12-08 22:05:07 +0000

    grading app works, also needs style

--- gradingapp.js
+++ gradingapp.js
@@ -3,8 +3,23 @@ function giveGrade() {
   var grade = document.getElementById("grade").value;
 
   console.log("we're here!");
-  alert("hey!");
+  
 
+if (grade <= 100 && grade >= 90){
+alert("A")
+}
+if (grade <= 89 && grade >= 80){
+alert("B")
+}
+if (grade <= 79 && grade >= 70){
+alert ("C")
+}
+if (grade <= 69 && grade >= 65){
+alert ("D")
+}
+if (grade <= 64 && grade >= 55){
+alert ("F")
+}
 //Input your code below:
 
   }
